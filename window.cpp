126349b5cf191621bd6e0bdab02b4fdcba397f9a
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2016-09-15 13:18:04 +0530

    adding window code

--- window.cpp
+++ window.cpp
@@ -2,5 +2,10 @@
 
 Window::Window(QWidget *parent) : QWidget(parent)
 {
+    // Set size of the window
+     setFixedSize(100, 50);
 
+     // Create and position the button
+     m_button = new QPushButton("Hello World", this);
+     m_button->setGeometry(10, 10, 80, 30);
 }
--- window.h
+++ window.h
@@ -3,15 +3,13 @@
 
 #include <QWidget>
 
+class QPushButton;
 class Window : public QWidget
 {
-    Q_OBJECT
 public:
     explicit Window(QWidget *parent = 0);
-
-signals:
-
-public slots:
+ private:
+ QPushButton *m_button;
 };
 
 #endif // WINDOW_H
