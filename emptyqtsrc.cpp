c8d2918305de4e308a04d046ca6f7fcc98c6e426
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2016-09-15 15:12:51 +0530

    slide up progress bar connect

--- emptyqtsrc.cpp
+++ emptyqtsrc.cpp
@@ -1,12 +1,40 @@
 #include <QApplication>
-#include "window.h"
+#include <QProgressBar>
+#include <QSlider>
 
 int main(int argc, char **argv)
 {
- QApplication app (argc, argv);
+    QApplication app (argc, argv);
 
- Window window;
- window.show();
+    // Create a container window
+    QWidget window;
+    window.setFixedSize(100 , 400);
 
- return app.exec();
+    // Create a progress bar
+    // with the range between 0 and 100, and a starting value of 0
+    QProgressBar *progressBar = new QProgressBar(&window);
+    progressBar->setRange(0, 100);
+    progressBar->setValue(0);
+    progressBar->setTextDirection(QProgressBar::TopToBottom);
+    progressBar->setOrientation(Qt::Vertical);
+    progressBar->setGeometry(10, 10, 30, 300);
+    progressBar->setFormat("%p%");
+    progressBar->setInvertedAppearance(true);
+
+    // Create a horizontal slider
+    // with the range between 0 and 100, and a starting value of 0
+    QSlider *slider = new QSlider(&window);
+    slider->setOrientation(Qt::Vertical);
+    slider->setRange(0, 100);
+    slider->setValue(0);
+    slider->setGeometry(40, 10, 30, 300);
+
+    window.show();
+
+    // Connection
+    // This connection set the value of the progress bar
+    // while the slider's value changes
+    QObject::connect(slider, SIGNAL (valueChanged(int)), progressBar, SLOT (setValue(int)));
+
+    return app.exec();
 }
