9b2c9f02e3188cc2a740b58db641346d3100ca30
s4p3r-c1n0s <umang.cdr.90@gmail.com>
2016-09-15 14:09:12 +0530

    using/calling code outside main

--- emptyqtsrc.cpp
+++ emptyqtsrc.cpp
@@ -1,22 +1,11 @@
 #include <QApplication>
-#include <QPushButton>
+#include "window.h"
 
 int main(int argc, char **argv)
 {
  QApplication app (argc, argv);
 
- QWidget window;
- window.setFixedSize(100, 50);
- QPushButton button ("Hello world !", &window);
- button.setText("My text");
- button.setToolTip("A tooltip");
- QFont font ("Courier", -1, -1, false);
- button.setFont(font);
- QPushButton *but = &button;
- but->setGeometry(10,20,80,30);
- //QIcon icon ("/home/umangjeet/Pictures/");
- button.setIcon(QIcon::fromTheme("face-smile"));
- QPushButton button2 ("other", &button);
+ Window window;
  window.show();
 
  return app.exec();
--- window.cpp
+++ window.cpp
@@ -1,4 +1,5 @@
 #include "window.h"
+#include <QPushButton>
 
 Window::Window(QWidget *parent) : QWidget(parent)
 {
